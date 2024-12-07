930806e822ca037b09afbeb7ff178c90a582bc20
Sonjoy Dey <95380764+sonjoy2074@users.noreply.github.com>
2024-12-07 21:53:48 +0600

    model created

--- App_login/models.py
+++ App_login/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE, related_name='user_profile') 
+    profile_pic = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
